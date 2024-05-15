78a03ed11bf2f5e2b06929a4d3c88d6225ee4891
angelinasantiago <77749659+angelinasantiago@users.noreply.github.com>
2024-05-15 18:08:52 -0400

    FINAL

--- js/my-js.js
+++ js/my-js.js
@@ -1,5 +1,16 @@
-
+// https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+   var x = document.getElementById("myDIV");
+   if (x.style.display === "block") {
+     x.style.display = "none";
+   } else {
+     x.style.display = "block";
+   }
+ }
+
+
+
+// function myFunction() {
+//    var element = document.body;
+//    element.classList.toggle("dark-mode");
+// }
